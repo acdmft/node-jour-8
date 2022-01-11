e2bb09ed42a6003262d829bf05017c4e69d33a33
fusbudget <andrewsanser@gmail.com>
2022-01-11 14:39:54 +0100

    add pokemon.js

---
+++ pokemon.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+function catchPokemon(pokemonId) {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/
+    `).then(function(res) {
+        console.log(`id: ${res.data.id}\nname: ${res.data.name}`);
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
+
+catchPokemon(5);
