0f8e763928214c9ec43a814c68e85ac6c00dc032
fusbudget <andrewsanser@gmail.com>
2022-01-12 10:20:08 +0100

    add some improvements in countries.js

--- countries.js
+++ countries.js
@@ -1,17 +1,14 @@
 const axios = require("axios");
 
-const countriesNames = [];
+let countriesNames = [];
 
 function getCountries() {
     axios.get("https://restcountries.com/v3.1/all")
-    .then(function(res) {
-        res.data.forEach(element => {
-            countriesNames.push(element.name.common);
-        });;
-        console.log(countriesNames);
-    })
-    .catch(function(err) {
-        console.error(err);
+        .then(function(res) {
+            countriesNames = res.data.map(function(country) {
+                return country.name.common;
+        });
+        console.log(countriesNames.join(' - '));
     });
 }
 
